c6bcdd8f1ef655fef4f6513b02a940607ea4d289
willi.preuk <willi@preuk.eu>
2020-05-22 17:37:32 +0200

    reference

--- src/main/kotlin/models/Group.kt
+++ src/main/kotlin/models/Group.kt
@@ -3,7 +3,7 @@ package models
 import org.jetbrains.exposed.dao.id.EntityID
 import java.time.LocalDateTime
 
-data class GroupModel(val id: Int, val name: String, val matchField: String, val createdAt: LocalDateTime, val updatedAt: LocalDateTime)
+data class GroupModel(val id: Int, val name: String, val matchField: String, val teams: List<TeamModel>, val createdAt: LocalDateTime, val updatedAt: LocalDateTime)
 
 object Groups : BaseIntIdTable("Groups") {
     val name = varchar("name", 50)
@@ -15,8 +15,9 @@ class Group(id: EntityID<Int>) : BaseIntEntity(id, Groups) {
 
     var name by Groups.name
     var matchField by Groups.matchField
+    val teams by Team referrersOn Teams.group
 
     fun toModel(): GroupModel {
-        return GroupModel(id.value, name, matchField, createdAt, updatedAt)
+        return GroupModel(id.value, name, matchField, teams.toList().map { it.toModel() }, createdAt, updatedAt)
     }
 }
